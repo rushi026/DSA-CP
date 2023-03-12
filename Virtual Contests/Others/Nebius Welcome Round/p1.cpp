caf8f000d8ae4eec4bb23a1e1954cd38846d42c3
rushi026 <rushiparekh026@gmail.com>
2023-03-12 23:23:22 +0530

    added new problems

---
+++ Virtual Contests/Others/Nebius Welcome Round/p1.cpp
@@ -0,0 +1,74 @@
+/*
+    # Virtual Contest 
+    Problem:            A. Lame King
+    Problem link:       https://codeforces.com/contest/1804/problem/A
+    Contest:            Nebius Welcome Round (Div. 1 + Div. 2)
+    Date:               12/03/2023
+    Last Updated:       12/03/2023
+    Author:             Rushiraj Parekh
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+/***********************************/
+
+#define ll                  long long int
+#define ull                 unsigned long long int
+#define loop(i,x,y,z)       for(ll i=x; i<y; i+=z)
+#define rloop(i,x,y,z)      for(ll i=x; i>=y; i-=z)
+#define ee                  '\n'
+#define vll                 vector<ll>
+#define scan(v)             for(auto &i : v) cin>>i;
+#define print(v)            for(auto i : v) cout<<i<<" ";
+#define pb                  push_back
+#define all(x)              x.begin(),x.end()
+#define _sort(x)            sort(all(x))
+#define speedy              ios::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+#define ins(vec,pos,val)    vec.emplace(vec.begin()+pos, val)
+#define del(vec,pos)        vec.erase(vec.begin()+pos)  
+
+/**********************************/
+
+ll power(ll x, ll y, ll p = 1e9+7) 
+{ 
+    ll res = 1;
+    x = x ;
+    while(y>0) 
+    { 
+        if(y&1) {
+            res = (res*x);
+        }
+        x = (x*x);
+        y = y>>1;
+    }
+    return res;
+}
+
+
+/***********************************/
+void solve()
+{
+    ll a,b;
+    cin>>a>>b;
+    a = abs(a);
+    b = abs(b);
+    if(a==b) cout<<a+b<<ee;
+    else cout<<a+b+abs(a-b)-1<<ee;
+}
+
+/***********************************/
+int main()
+{
+    #ifndef ONLINE_JUDGE
+        freopen("../../../input.txt", "r", stdin);
+        freopen("../../../output.txt", "w", stdout);
+    #endif
+    
+    speedy;
+    ll t=1;
+    cin>>t;
+    while(t--)
+        solve();
+    return 0;
+}
