ba6f5973b717c45af9f54df01c1ff0450cacd013
Rushiraj Parekh <rushiparekh026@gmail.com>
2023-04-26 14:27:33 +0530

    Round 867: Problem D

---
+++ Contests/Codeforces Round/Round-867/p4.cpp
@@ -0,0 +1,77 @@
+/*
+    Problem:            D. Super-Permutation
+    Problem link:       https://codeforces.com/contest/1822/problem/D
+    Contest:            Codeforces Round 867 (Div. 3)
+    Date:               26/04/2023
+    Last Updated:       26/04/2023
+    Author:             Rushiraj Parekh
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+/***********************************/
+
+#define ll                  long long int
+#define ull                 unsigned long long int
+#define loop(i,x,y,z)       for(i=x; i<y; i+=z)
+#define rloop(i,x,y,z)      for(i=x; i>=y; i-=z)
+#define ee                  '\n'
+#define vll                 vector<ll>
+#define scan(v)             for(auto &i : v) cin>>i;
+#define print(v)            for(auto i : v) cout<<i<<" ";
+#define pb                  push_back
+#define all(x)              x.begin(),x.end()
+#define _sort(x)            sort(all(x))
+#define speedy              ios::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+#define ins(vec,pos,val)    vec.emplace(vec.begin()+pos, val)
+#define del(vec,pos)        vec.erase(vec.begin()+pos)  
+#define sll                	set<ll>
+#define pll               	pair<ll, ll>
+#define mll                	map<ll, ll>
+
+/**********************************/
+
+ll pow(ll x, ll n) {
+    ll res = 1;
+    while(n){
+        if(n&1) res *= x;
+        x *= x;
+        n >>= 1;
+    }
+    return res;
+}
+
+
+/***********************************/
+void solve() {
+    ll n,i;
+    cin>>n;
+    if(n == 1) {
+        cout<<1<<ee;
+        return;
+    }
+    if(n&1) {
+        cout<<-1<<ee;
+        return;
+    }
+    loop(i,1,n+1,1) {
+        if(i&1) cout<<n-i+1<<" ";
+        else cout<<i-1<<" ";
+    }
+    cout<<ee;
+}
+
+/***********************************/
+int main() {
+    #ifndef ONLINE_JUDGE
+        freopen("../../../input.txt", "r", stdin);
+        freopen("../../../output.txt", "w", stdout);
+    #endif
+
+    speedy;
+    ll t=1;
+    cin>>t;
+    while(t--) solve();
+    return 0;
+}
