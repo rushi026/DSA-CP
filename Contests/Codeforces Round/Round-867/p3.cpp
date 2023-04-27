bb1fafb5fa8f7a854c462400638ab30de81c5d1e
Rushiraj Parekh <rushiparekh026@gmail.com>
2023-04-27 20:03:18 +0530

    Round 867: Problem A,B,C

---
+++ Contests/Codeforces Round/Round-867/p3.cpp
@@ -0,0 +1,66 @@
+/*
+    Problem:            C. Bun Lover
+    Problem link:       https://codeforces.com/contest/1822/problem/C
+    Contest:            Codeforces Round 867 (Div. 3)
+    Date:               24/04/2023
+    Last Updated:       24/04/2023
+    Author:             Rushiraj Parekh
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+/***********************************/
+
+#define ll                  long long int
+#define ull                 unsigned long long int
+#define loop(i,x,y,z)       for(i=x; i<y; i+=z)
+#define rloop(i,x,y,z)      for(i=x; i>=y; i-=z)
+#define ee                  '\n'
+#define vll                 vector<ll>
+#define scan(v)             for(auto &i : v) cin>>i;
+#define print(v)            for(auto i : v) cout<<i<<" ";
+#define pb                  push_back
+#define all(x)              x.begin(),x.end()
+#define _sort(x)            sort(all(x))
+#define speedy              ios::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+#define ins(vec,pos,val)    vec.emplace(vec.begin()+pos, val)
+#define del(vec,pos)        vec.erase(vec.begin()+pos)  
+#define sll                	set<ll>
+#define pll               	pair<ll, ll>
+#define mll                	map<ll, ll>
+
+/**********************************/
+
+ll pow(ll x, ll n) {
+    ll res = 1;
+    while(n){
+        if(n&1) res *= x;
+        x *= x;
+        n >>= 1;
+    }
+    return res;
+}
+
+
+/***********************************/
+void solve() {
+    ll n, a=26, d=11, c=2;
+    cin>>n;
+    n -= 3;
+    cout<<a+(n-1)*d + (n-1)*(n-2)/2*c<<ee;
+}
+
+/***********************************/
+int main() {
+    #ifndef ONLINE_JUDGE
+        freopen("../../../input.txt", "r", stdin);
+        freopen("../../../output.txt", "w", stdout);
+    #endif
+
+    speedy;
+    ll t=1;
+    cin>>t;
+    while(t--) solve();
+    return 0;
+}
